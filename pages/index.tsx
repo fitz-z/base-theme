083ab0f7ce1e997f32e9ad4e35ab80339cf56477
Sean Fitzgerald <sfitzgerald107@gmail.com>
2021-04-19 21:06:04 -0400

    bootstrapped nextjs and tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+const IndexPage = () => {
+  return (
+    <div className="w-full h-full min-h-screen flex justify-center items-center">
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
