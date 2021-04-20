7cd46058e9a925f9bbb7869d440e67fd5d105e6f
Sean Fitzgerald <sfitzgerald107@gmail.com>
2021-04-19 22:00:27 -0400

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-1": "#355c7d",
+        "primary-2": "#6c5b7b",
+        "secondary-1": "#c06c84",
+        "secondary-2": "#f67280",
+        "tertiary-1": "#f8b195",
+      },
+    },
   },
   variants: {
     extend: {},
